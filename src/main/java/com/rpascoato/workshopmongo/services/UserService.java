9a7e9e5686129b1f3feba0842a0307ac4c7ad7d1
Raphael Pascoato <rapha367@live.com>
2021-11-09 08:56:10 -0300

    Obtendo um usuario por id

--- src/main/java/com/rpascoato/workshopmongo/services/UserService.java
+++ src/main/java/com/rpascoato/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rpascoato.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rpascoato.workshopmongo.domain.User;
 import com.rpascoato.workshopmongo.repository.UserRepository;
+import com.rpascoato.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
