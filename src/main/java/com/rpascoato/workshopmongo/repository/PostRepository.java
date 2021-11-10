df519f3ab6dd1cb7fd6e7268dd24ec599ac4d6e9
Raphael Pascoato <rapha367@live.com>
2021-11-10 20:38:42 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/rpascoato/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.rpascoato.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rpascoato.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
