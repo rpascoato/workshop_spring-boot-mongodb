5cf1cd6a1d97f20690c38d37f87d9b5e6b2693b7
Raphael Pascoato <rapha367@live.com>
2021-11-11 08:27:33 -0300

    Obtendo um post por id

---
+++ src/main/java/com/rpascoato/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.rpascoato.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rpascoato.workshopmongo.domain.Post;
+import com.rpascoato.workshopmongo.repository.PostRepository;
+import com.rpascoato.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
